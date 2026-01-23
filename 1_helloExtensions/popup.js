526ee359e240b4fe8dd1ed2f0fa301f70aaf4d8e
Osama-alt-del <abdalghanyosama@gmail.com>
2026-01-23 14:19:17 +1030

    First popup extension!

---
+++ 1_helloExtensions/popup.js
@@ -0,0 +1,4 @@
+// This is javascript code that shows that prints out that this is a popup!
+
+// Sure enough, this pops up in the console part when we inspect the popup
+console.log("This is a popup!");
