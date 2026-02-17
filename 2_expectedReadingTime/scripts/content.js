373ee762e6e82ce41c4d6c39aac5ca8a265a10fc
Osama-alt-del <abdalghanyosama@gmail.com>
2026-02-18 02:26:21 +1030

    reading time wip

---
+++ 2_expectedReadingTime/scripts/content.js
@@ -0,0 +1,14 @@
+// This is a JS file where I will calculate the reading time of Chrome developer articles, and webstore pages
+
+function renderReadingTime(article) {
+	// If no article, don't render anything
+	if (!article) {
+		return;
+	}
+
+	const text = article.textContent;
+	const wordMatchRegExp = /[^\s]+/g; // What is this?
+
+
+}
+
