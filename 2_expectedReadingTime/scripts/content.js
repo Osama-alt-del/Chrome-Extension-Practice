a9bd344d3b9a8486efcc37d907ced99887819ba8
Osama-alt-del <abdalghanyosama@gmail.com>
2026-02-18 02:48:22 +1030

    reading time wip

--- 2_expectedReadingTime/scripts/content.js
+++ 2_expectedReadingTime/scripts/content.js
@@ -8,6 +8,16 @@ function renderReadingTime(article) {
 
 	const text = article.textContent;
 	const wordMatchRegExp = /[^\s]+/g; // What is this?
+	const words = text.matchAll(wordMatchRegExp); // ?
+	// matchAll returns an iterator, convert to array to get word count 
+	const wordCount = [...words].length; // ?
+	const readingTime = Math.round(wordCount / 200); 
+	const badge = document.createElement("p");
+	// Use the same styling as the publish information in the article's header ??
+	badge.classList.add("color-secondary-text", "type--caption"); //?
+	badge.textContent = `⏱️ ${readingTime} min read`; //?
+
+	
 
 
 }
