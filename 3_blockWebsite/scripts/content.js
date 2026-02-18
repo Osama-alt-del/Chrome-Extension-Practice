c020f18c667448e9e2255683d1639ec7b1e510f0
Osama-alt-del <abdalghanyosama@gmail.com>
2026-02-19 00:25:59 +1030

    Working on block site extension to block any and all sites that I want to block

---
+++ 3_blockWebsite/scripts/content.js
@@ -0,0 +1,8 @@
+/* 
+This is a JS file that I will use so that whenever I head to a site that I don't want to use anymore, that site's html will change to 
+a page that says "cannot use this site" for example
+*/
+
+// Take in the article and then change the "html" inner html to be something that you make 
+function blockSite(site){
+}
